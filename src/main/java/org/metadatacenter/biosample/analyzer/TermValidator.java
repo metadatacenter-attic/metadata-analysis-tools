51d785f201435ffbc0b56bafe909efe8f4188c8e
Laura Miron <lauramiron@DN51s58n.SUNet>
2019-07-28 16:39:19 -0700

    find all ontology matches

--- src/main/java/org/metadatacenter/biosample/analyzer/TermValidator.java
+++ src/main/java/org/metadatacenter/biosample/analyzer/TermValidator.java
@@ -18,6 +18,7 @@ import java.util.regex.Pattern;
 import static com.google.common.base.Preconditions.checkNotNull;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.FileReader;
@@ -89,7 +90,13 @@ public final class TermValidator {
       ArrayList<String> cuis = new ArrayList<String>();
       ArrayList<String> tuis = new ArrayList<String>();
       JsonNode cuiNode = node.get("cui");
+      if (cuiNode == null) {
+        cuiNode = node.get("UMLS_CUI");
+      }
       JsonNode tuiNode = node.get("tui");
+      if (tuiNode == null) {
+        tuiNode = node.get("Semantic_Type");
+      }
       if (cuiNode != null) {
         for (JsonNode objNode : cuiNode) {
           cuis.add(objNode.textValue());
@@ -104,6 +111,81 @@ public final class TermValidator {
     } else {
       return new TermValidationReport("", "", false, false, false, "", null, null);
     }
+  }
+
+  public ArrayList<TermValidationReport> validateTermMulti(@Nonnull String term, boolean exactMatch, @Nonnull String... ontologies) {
+    String searchString = p1.matcher(term).replaceAll("+");
+    searchString = p2.matcher(searchString).replaceAll("");
+    searchString = p3.matcher(searchString).replaceAll("");
+
+    Optional<JsonNode> searchResult = Optional.empty();
+    if (!searchString.trim().isEmpty()) {
+      if (ontologies.length > 0) {
+        String onts = "";
+        for (int i = 0; i < ontologies.length; i++) {
+          onts += ontologies[i] + (i == ontologies.length - 1 ? "" : ",");
+        }
+        searchResult = bioPortalAgent.getResult(searchString, exactMatch, onts);
+      } else {
+        searchResult = bioPortalAgent.getResult(searchString, exactMatch);
+      }
+    }
+
+    ArrayList<TermValidationReport> results = new ArrayList<TermValidationReport>();
+    if (!searchResult.isPresent() || !searchResult.get().elements().hasNext()) {
+      results.add(new TermValidationReport("", "", false, false, false, "", null, null));
+      return results;
+    }
+    
+    // int numResults = 0;
+    Iterator<JsonNode> iter = searchResult.get().elements();
+    while (iter.hasNext()) {
+      // if (numResults >=6) break;
+      // numResults +=1;
+
+      // System.out.println(numResults);
+
+      // look at the first result from BioPortal
+      JsonNode node = iter.next();
+      // System.out.println(node.asText());
+
+      String type = node.get("@type").textValue();
+      boolean isOWLClass = isOwlClass(type);
+
+      String ontologyType = node.get("ontologyType").textValue();
+      boolean isOntology = isOntology(ontologyType);
+
+      String value = node.get("@id").textValue();
+      String label = node.get("prefLabel").textValue();
+
+      String ontology = "";
+      JsonNode ontologyNode = node.get("ontology");
+      if (ontologyNode != null) {
+        ontology = ontologyNode.textValue();
+      }
+      ArrayList<String> cuis = new ArrayList<String>();
+      ArrayList<String> tuis = new ArrayList<String>();
+      JsonNode cuiNode = node.get("cui");
+      if (cuiNode == null) {
+        cuiNode = node.get("UMLS_CUI");
+      }
+      JsonNode tuiNode = node.get("tui");
+      if (tuiNode == null) {
+        tuiNode = node.get("Semantic_Type");
+      }
+      if (cuiNode != null) {
+        for (JsonNode objNode : cuiNode) {
+          cuis.add(objNode.textValue());
+        }
+      }
+      if (tuiNode != null) {
+        for (JsonNode objNode : tuiNode) {
+          tuis.add(objNode.textValue());
+        }
+      }
+      results.add(new TermValidationReport(value,label,isOntology,isOWLClass,true,ontology,cuis,tuis));
+    }
+    return results;
   }
 
   private boolean isOntology(@Nonnull String ontologyType) {
@@ -168,7 +250,7 @@ public final class TermValidator {
   /* Main */
   public static void OutputResult(TermValidationReport report, FileWriter fw, String idx, String term) throws IOException {
     if (fw != null) {
-      fw.write(idx+"\t"+term+"\t"+report.getMatchValue()+"\t"+report.getMatchLabel()+"\t"+report.getCuis()+"\t"+report.getSemanticTypes()+"\n");
+      fw.write(idx+"\t"+term+"\t"+report.getMatchValue()+"\t"+report.getMatchLabel()+"\t"+report.getCuis()+"\t"+report.getSemanticTypes()+"\t"+report.getOntology()+"\n");
     } else {
       System.out.println(term + " " + report.toString());
     }
@@ -199,6 +281,8 @@ public final class TermValidator {
     CommandLine cmd = parser.parse(options,args);
 
     boolean exactMatch = (cmd.hasOption("em")) ? Boolean.parseBoolean(cmd.getOptionValue("em")) : true;
+    // boolean allResults = (cmd.hasOption("ar")) ? Boolean.parseBoolean(cmd.getOptionValue("ar")) : false;
+    boolean allResults = true;
     String bioPortalApiKey = cmd.getOptionValue("k");
     String [] ontology = (cmd.hasOption("o")) ? cmd.getOptionValue("o").split(",") : null;
     // boolean allReults = (cmd.hasOption("ar")) ? true : false;
@@ -243,16 +327,27 @@ public final class TermValidator {
       }
 
       TermValidationReport report;
+      ArrayList<TermValidationReport> reports = new ArrayList<TermValidationReport>();
       int num_retries = 0;
       while (true) {
         try {
+          if (allResults==true) {
+            reports = validator.validateTermMulti(term, exactMatch);
+            for (int j=0; j<reports.size(); j++) {
+              OutputResult(reports.get(j),fw,idx,term);
+            }
+            break;
+          }
+
           if (ontology != null) {
             report = validator.validateTerm(term, exactMatch, ontology);
           } else {
             report = validator.validateTerm(term,exactMatch);
           }
+          System.out.println("shouldn't be here");
           OutputResult(report, fw, idx, term);
           break;
+
         } catch (Exception e) {
           if (num_retries >= 5) {
             System.out.println("Too many retries, skipping "+term);
